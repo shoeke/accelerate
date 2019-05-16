92fe4f216fdd1a6a8a542c521cbfc60418fd020f
sabrina <hoekehouse@gmail.com>
2019-05-15 21:54:21 -0700

    custom fields for custom post

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -17,7 +17,21 @@ get_header(); ?>
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+        $services = get_field('services'),
+        $client = get_field('client'),
+        $link = get_field('site_link'),
+        $image_1 = get_field('image_1'),
+        $image_2 = get_field('image_2'),
+        $image_3 = get_field('image_3'); ?>
+
+        <aside class="case-study-sidebar">
+          <h2><?php the_title(); ?></h2>
+          <h5><?php echo $services; ?></h5>
+          <h6><?php echo $client; ?></h6>
+          <?php the_content(); ?>
+
+          <p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site</a></p>
+        </aside>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
