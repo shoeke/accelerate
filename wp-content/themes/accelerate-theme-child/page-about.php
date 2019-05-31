a90243854daf6671f66a7317dbd3dd221278a053
sabrina <hoekehouse@gmail.com>
2019-05-31 11:47:31 -0700

    about page alternating floats

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -17,27 +17,38 @@ Accelerate is a strategy and marketing agency located in the heart of NYC. Our g
 </header>
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
-		<h2>Our Services</h2>
-		<p>We take pride in our clients and the content we create for them.
-		Here’s a brief overview of our offered services.</p>
+			<div class="center-text">
+				<h2>Our Services</h2>
+				<p>We take pride in our clients and the content we create for them.
+				<br>Here’s a brief overview of our offered services.</p>
+			</div>
 
-		<?php 			query_posts('post_type=services'); ?>
 
-				<?php while ( have_posts() ) : the_post();
-					$service = get_field('service');
-					$service_descr = get_field('service_descr');
-					$image_1 = get_field('image_icon');
-					$size = "medium";
-					?>
-					<div class="">
+	<?php 			query_posts('post_type=services'); ?>
+
+			<?php while ( have_posts() ) : the_post();
+				$service = get_field('service');
+				$service_descr = get_field('service_descr');
+				$image_1 = get_field('image_icon');
+				$size = "medium";
+				?>
+				<div class="service-loop-wrap">
+					<div class="service-text">
 						<h2><?php the_title(); ?></h2>
-						<p><?php the_content(); ?></p>
+						<p><?php echo $service_descr; ?></p>
+					</div>
+					<div class="service-img">
 						<img src=<?php if($image_1) {
 							echo wp_get_attachment_image( $image_1, $size );
 						}?>
 					</div>
+					<div class="clearfix-about"></div>
+				</div>
 
-			<?php endwhile; // end of the loop. ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+
 		</div><!-- .main-content -->
 
 
